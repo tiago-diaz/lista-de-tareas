07f5921b90ba81bf1e2c4d098be3c56009c7a2ef
Tiago <tiago.diaz086@outlook.com>
2025-10-11 12:57:10 -0300

    agrega comentarios y docstrings

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-tareas = []
+tareas = [] # lista global para almacenar tareas
 
 def mostrar_tareas():
-
+    """muestra las tareas pendientes"""
     if tareas:
         print("\n--- TAREAS PENDIENTES ---")
         for i, tarea in enumerate(tareas, 1):
@@ -10,6 +10,7 @@ def mostrar_tareas():
         print("No hay tareas pendientes.")
 
 def agregar_tarea():
+    """agrega una nueva tarea a la lista"""
     nueva_tarea = input("Escriba el nombre de la tarea a agregar: ")
     
     if nueva_tarea:
@@ -19,6 +20,7 @@ def agregar_tarea():
         print("El nombre de la tarea no puede estar vacío.")
 
 def eliminar_tarea():
+    """elimina una tarea por su índice"""
     mostrar_tareas()
 
     try:
@@ -29,9 +31,11 @@ def eliminar_tarea():
         else:
             print("Número fuera de rango.")
     except ValueError:
+        # evita que el usuario ingrese texto o símbolos en lugar de un número
         print("Por favor, ingrese un número válido.")
 
 def main():
+    """muestra el menú de opciones"""
     while True:
         print("\n--- MENÚ DE TAREAS ---")
         print("1. Mostrar tareas")
@@ -50,5 +54,6 @@ def main():
         elif op == "4":
             print("Saliendo del programa...")
             break
+
 if __name__ == "__main__":
     main()
