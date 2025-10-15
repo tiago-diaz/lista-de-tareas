1a0c9100ad82e201119b3b20988e9f059609b461
Tiago <tiago.diaz086@outlook.com>
2025-10-15 20:59:42 -0300

    almacenar tareas en json

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+import json
+
 tareas = [] # lista global para almacenar tareas
+
+def cargar_tareas():
+    """carga las tareas del json"""
+    global tareas
+    try:
+        with open("tareas.json", "r", encoding="UTF-8") as archivo:
+            try:
+                tareas = json.load(archivo)
+
+            except json.decoder.JSONDecodeError:
+                with open("tareas.json", "w", encoding="UTF-8") as archivo:
+                    json.dump([], archivo, indent=4)
+
+    except FileNotFoundError:
+        with open("tareas.json", "w", encoding="UTF-8") as archivo:
+            json.dump([], archivo, indent=4)
+
+def guardar_tareas():
+    """guarda las tareas en el json"""
+    global tareas
+    with open("tareas.json", "w", encoding="UTF-8") as archivo:
+        json.dump(tareas, archivo, indent=4)
 
 def mostrar_tareas():
     """muestra las tareas pendientes"""
@@ -15,18 +39,22 @@ def agregar_tarea():
     
     if nueva_tarea:
         tareas.append(nueva_tarea)
+        guardar_tareas()
         print("Tarea agregada con éxito.")
     else:
         print("El nombre de la tarea no puede estar vacío.")
 
 def eliminar_tarea():
     """elimina una tarea por su índice"""
+    if not tareas:
+        print("No hay tareas pendientes.")
+        return
     mostrar_tareas()
-
     try:
         tarea = int(input("Escriba el índice de la tarea a eliminar: "))
         if 1 <= tarea <= len(tareas):
             tareas.pop(tarea-1)
+            guardar_tareas()
             print("Tarea eliminada con éxito.")
         else:
             print("Número fuera de rango.")
@@ -36,6 +64,7 @@ def eliminar_tarea():
 
 def main():
     """muestra el menú de opciones"""
+    cargar_tareas()
     while True:
         print("\n--- MENÚ DE TAREAS ---")
         print("1. Mostrar tareas")
